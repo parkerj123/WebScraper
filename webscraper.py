664aef44093cdbb93ed1f8cd623697e40f5624b2
Parker Janke <parkerjanke1@gmail.com>
2019-12-06 15:14:17 -0800

    update webscrapper.py

--- webscraper.py
+++ webscraper.py
@@ -4,7 +4,9 @@ from bs4 import BeautifulSoup as bs4
 import urllib3
 import certifi
 import os,sys
-import argparse 
+import argparse
+from collections import defaultdict
+import csv 
 
 
 parser = argparse.ArgumentParser(description="Scrape a website")
@@ -33,6 +35,9 @@ else:
 if args.t is None:
 	tags = False
 
+titles_raw = []
+titles_formatted = defaultdict(lambda :0)
+
 
 
 # Used to make requests, takes care of all pooling and thread safety
@@ -57,9 +62,51 @@ soup = bs4(content,features="html.parser")
 #print(soup.body.get_text())
 
 
-if tags == True:
-	for lines in soup.find_all(args.t):
-		print("%s -- class: %s" % (lines.name, lines.get('class')))
-else:
-	print(soup.prettify())
+# if tags == True:
+# 	for lines in soup.find_all(args.t):
+# 		print(lines)
+# else:
+# 	print(soup.prettify())
+bad_rex = ['!','@','#','$','%','^','&','*','+','=','{','}','|','[',']','\\',';','\"',':','\'','(',')']
+
+
+stuff = soup.find_all('div')
+for s in stuff:
+	#print(s.get('class'))
+	if (s.get('class') != None) and (s.get('class')[0] == "blog-title-2"):
+		#print(s.string)
+		titles_raw.append(s.string)
+	#else:
+	#	print('Not this one')
+
+#print(titles_raw)
+
+for titles in titles_raw:
+	break_it = False
+	title = titles.lower().split()
+	for new_title in title:
+		for char in new_title:
+			if char in bad_rex:
+				i = new_title.find(char)
+				new_title = new_title[0:i]
+				titles_formatted[new_title]+=1
+				break_it = True
+				break;
+		titles_formatted[new_title]+=1
+
+
+
+#for keys in titles_formatted.keys():
+#	print(keys)
+
+with open('scrapper.csv', mode='w') as scraperCSV:
+	fieldNames = ['word', 'count']
+	writer = csv.DictWriter(scraperCSV, fieldnames=fieldNames)
+	writer.writeheader()
+
+	for k, v in titles_formatted.items():
+		writer.writerow({'word': k, 'count': v})
+
+
+
 
