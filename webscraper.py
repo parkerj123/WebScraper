e58ef8f1e6cec9e56805806bf5f9d65d897f2589
Parker <parker@metricfire.com>
2020-01-07 11:25:02 -0800

    updat webscraper for WGU

--- webscraper.py
+++ webscraper.py
@@ -7,7 +7,6 @@ import argparse
 from collections import defaultdict
 import csv
 
-
 '''
 
 This is a docstring example.
@@ -32,6 +31,7 @@ parser.add_argument('-t',
 
 args = parser.parse_args()
 
+BASE_PATH = "https://census.gov"
 DEFAULT_URL = "https://www.census.gov/programs-surveys/popest.html"
 url = ''
 tags = True
@@ -44,8 +44,8 @@ else:
 if args.t is not None:
     tags = True
 
-titles_raw = []
 titles_formatted = defaultdict(lambda: 0)
+
 
 # Used to make requests, takes care of all pooling and thread safety
 lib = urllib3.PoolManager(cert_reqs='CERT_REQUIRED',
@@ -53,10 +53,30 @@ lib = urllib3.PoolManager(cert_reqs='CERT_REQUIRED',
 
 # Get URL data
 content = lib.request('GET', url).data
-
 soup = bs4(content, features="html.parser")
+html_data = soup.find_all(args.t)
+for link in html_data:
+    if link.get('href') is not None:
+        if link.get('href')[0] == "/":
+            href = BASE_PATH + link.get('href')
+            if href.endswith("/"):
+                titles_formatted[href[:-1]] = 1
+            else:
+                titles_formatted[href] = 1
+        elif link.get('href').find('#') != -1:
+            if link.get('href').find('#') > 0:
+                titles_formatted[link.get('href')[:link.get('href').find('#')]] = 1
+        else:
+            if link.get("href").endswith("/"):
+                titles_formatted[link.get("href")[:-1]] = 1
+            else:
+                titles_formatted[link.get("href")] = 1
+    else:
+        continue
 
-
-stuff = soup.find_all(args.t)
-for s in stuff:
-    print(s.get('href'))
+with open('links.csv', mode='w') as linksCSV:
+    fieldNames = ['link', 'count']
+    writer = csv.DictWriter(linksCSV, fieldnames=fieldNames)
+    writer.writeheader()
+    for k, v in titles_formatted.items():
+        writer.writerow({'link': k, 'count': v})
